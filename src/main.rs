5abe91bc8852cedf924062d86cf5788190a8d34d
Sebastian14 <sebasduarte1214uran@gmail.com>
2026-03-18 10:57:02 -0500

    feat: implement Repository with add, commit, and log commands

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,28 @@ mod repository;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
+    let mut repo = repository::Repository::new();
 
-    match command::parse(&args) {
-        Ok(cmd) => println!("Comando reconocido: {:?}",cmd),
-        Err(e) => eprintln!("Error: {}",e),
+    // Dividir args en sub-comandos separados por "/"
+    // cargo run -- add main.rs / commit "primer commit" / log
+    let commands: Vec<&[String]> = args[1..]
+        .split(|a| a == "/")
+        .collect();
+
+    for cmd_args in commands {
+        let full = std::iter::once(&args[0])
+            .chain(cmd_args.iter())
+            .cloned()
+            .collect::<Vec<String>>();
+
+        match command::parse(&full) {
+            Ok(cmd) => match cmd {
+                command::Command::Init       => println!("Repositorio inicializado."),
+                command::Command::Add(f)     => repo.add(f),
+                command::Command::Commit(m)  => repo.commit(m),
+                command::Command::Log        => repo.log(),
+            },
+            Err(e) => eprintln!("Error: {}", e),
+        }
     }
 }
--- src/repository.rs
+++ src/repository.rs
@@ -1,3 +1,4 @@
+
 // Un blob representa el contenido de un archivo 
 
 pub struct Blob{
@@ -30,4 +31,59 @@ pub enum GitObject{
     Blob(Blob),
     Tree(Tree),
     Commit(Commit),
+}
+
+use core::hash;
+use std::collections::HashMap;
+
+pub struct Repository{
+    pub objects: HashMap<String,GitObject>,
+    pub stanging: Vec<String>,
+    pub head: Option<String>,
+}
+
+impl Repository {
+    pub fn new() -> Repository {
+        Repository { objects: HashMap::new(),
+            stanging: Vec::new(), 
+            head: None,
+        }
+    }
+    pub fn add(&mut self, filename: String) {
+        println!("Agregando {} al staging...",filename);
+        self.stanging.push(filename);
+    }
+    pub  fn commit(&mut self, message: String) {
+        if self.stanging.is_empty() {
+            println!("Nada en el staging. Usa add primero.");
+            return;
+        }
+        let hash = format!("{:x}",self.stanging.join("").len());
+
+        let commit = Commit{
+            message,
+            author: "dev".to_string(),
+            tree_hash: hash.clone(),
+            parent_hash: self.head.clone()
+        };
+        
+
+        self.objects.insert(hash.clone(), GitObject::Commit(commit));
+        self.head = Some(hash.clone());
+        self.stanging.clear();
+
+        println!("Commit creado: {}", hash);
+    }
+    pub fn log(&self) {
+    match &self.head {
+        None => println!("No hay commits aún."),
+        Some(hash) => {
+            if let GitObject::Commit(c) = &self.objects[hash] {
+                println!("Commit: {}", hash);
+                println!("  Autor:   {}", c.author);
+                println!("  Mensaje: {}", c.message);
+                }
+            }
+        }   
+    }
 }
